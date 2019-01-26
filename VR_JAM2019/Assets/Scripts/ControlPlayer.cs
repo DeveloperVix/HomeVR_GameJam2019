42b6d4e0474bcb10180abd894447a2797f3ae09f
DeveloperVix <vicomisterio@hotmail.com>
2019-01-26 14:28:08 -0600

    VR Project
    The project.

---
+++ VR_JAM2019/Assets/Scripts/ControlPlayer.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlPlayer : MonoBehaviour
+{
+    public Text txtAxis;
+    public Text txtButton;
+
+ 
+    public float speedMove = 5f;
+
+    GameObject theCamera;
+
+    float axisY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theCamera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #region Joystick en @C
+        /*Para la configuración del joystick en @ C
+          * joystick button 0->C
+          */
+        if (Input.GetButtonDown("ButtonC_OnC"))//0
+        {
+            Debug.Log("presiono C");
+            //transform.Rotate(Vector3.left * spinValue * Time.deltaTime);
+            ShowButton("C");
+        }
+
+        if (Input.GetButtonDown("ButtonA_OnC"))//1
+        {
+            Debug.Log("presiono A");
+            ShowButton("A");
+        }
+
+        if (Input.GetButtonDown("ButtonB_OnC"))//2
+        {
+            Debug.Log("presiono B");
+            ShowButton("B");
+        }
+
+        if (Input.GetButtonDown("ButtonD_OnC"))//3
+        {
+            Debug.Log("presiono D");
+            ShowButton("D");
+        }
+
+        if (Input.GetButtonDown("ButtonRT_OnC"))//4
+        {
+            Debug.Log("presiono RT");
+            ShowButton("RT");
+        }
+
+        if (Input.GetButtonDown("ButtonRB_OnC"))//5
+        {
+            Debug.Log("presiono RB");
+            ShowButton("RB");
+        }
+
+        //Debug.Log(Input.GetAxis("Vertical"));
+        //Debug.Log(Input.GetAxis("Horizontal"));
+
+        txtAxis.text = "X: " + Input.GetAxis("Horizontal") + "   Y: " + Input.GetAxis("Vertical");
+        #endregion
+
+        axisY = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * speedMove * axisY * Time.deltaTime);
+
+        Vector3 eulerRotation = new Vector3(transform.eulerAngles.x, theCamera.transform.localEulerAngles.y + 5f, transform.eulerAngles.z);
+
+        transform.rotation = Quaternion.Euler(eulerRotation);
+    }
+
+    void ShowButton(string theButton)
+    {
+        txtButton.text = "Presiono " + theButton;
+    }
+}
---
+++ VR_JAM2019/Assets/Scripts/StatusObj.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatusObj : MonoBehaviour
+{
+    public enum Status { Idle, Taking};
+    public Status currentStatus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayerTakeMe()
+    {
+        currentStatus = Status.Taking;
+    }
+
+    public void Idle()
+    {
+        currentStatus = Status.Idle;
+    }
+}
